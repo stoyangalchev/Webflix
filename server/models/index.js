fbf6320d57cc5533d9eaa6f9636710b7e582d3a0
stoyangalchev <galchev98@gmail.com>
2024-03-08 16:57:21 +0200

    model indexjs update

---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+const userModel = require('./userModel');
+const tokenBlacklistModel = require('./tokenBlacklistModel');
+const themeModel = require('./themeModel');
+const postModel = require('./postModel');
+const commentModel = require('./commentModel');
+const movieModel = require('./movieModel')
+
+module.exports = {
+    userModel,
+    tokenBlacklistModel,
+    themeModel,
+    postModel,
+    commentModel,
+    movieModel
+}
