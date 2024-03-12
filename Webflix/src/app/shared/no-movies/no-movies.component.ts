1e2e897e0afee4bbdad2f64814d9fa2c51c3d6ba
stoyangalchev <galchev98@gmail.com>
2024-03-12 08:02:07 +0200

    Create no-movies.component.ts

---
+++ Webflix/src/app/shared/no-movies/no-movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-movies',
+  templateUrl: './no-movies.component.html',
+  styleUrls: ['./no-movies.component.css']
+})
+export class NoMoviesComponent {
+
+}
