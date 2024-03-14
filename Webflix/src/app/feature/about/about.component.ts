c1602f34bdf2b7c894845b4032a13e5379383c5a
stoyangalchev <galchev98@gmail.com>
2024-03-14 15:59:10 +0200

    Create about.component.ts

---
+++ Webflix/src/app/feature/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
