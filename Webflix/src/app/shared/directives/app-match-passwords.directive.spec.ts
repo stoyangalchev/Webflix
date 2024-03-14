1666e24acdd48f3b19c56b1a137cb127ac613ac7
stoyangalchev <galchev98@gmail.com>
2024-03-14 10:52:35 +0200

    Create app-match-passwords.directive.spec.ts

---
+++ Webflix/src/app/shared/directives/app-match-passwords.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AppMatchPasswordsDirective } from './app-match-passwords.directive';
+
+describe('AppMatchPasswordsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AppMatchPasswordsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
