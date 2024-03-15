54efbd73e774d2970ebd256e521d5490bfa11b75
stoyangalchev <galchev98@gmail.com>
2024-03-15 10:22:14 +0200

    Update movie-details.component.ts

--- Webflix/src/app/feature/movie-details/movie-details.component.ts
+++ Webflix/src/app/feature/movie-details/movie-details.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { MovieService } from '../services/movie.service';
-import { Movie } from 'src/app/Types/Movie';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ModalService } from '../services/modal.service';
-import { UserService } from '../services/user.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { MovieService } from "../services/movie.service";
+import { Movie } from "src/app/Types/Movie";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ModalService } from "../services/modal.service";
+import { UserService } from "../services/user.service";
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.component.html",
+  styleUrls: ["./movie-details.component.css"],
 })
-export class MovieDetailsComponent implements OnInit {
-
+export default class MovieDetailsComponent implements OnInit {
   comments: any;
   movie: Movie | undefined;
   movieId = this.route.snapshot.params["id"];
@@ -29,30 +26,40 @@ export class MovieDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.movieService.getMovie(this.movieId).subscribe(movie => {
+    this.movieService.getMovie(this.movieId).subscribe((movie) => {
       this.movie = movie;
       this.comments = movie.comments;
-      this.userService.user$.subscribe((user)=>{
-        if(user){
+      this.userService.user$.subscribe((user) => {
+        if (user) {
           this.isLoggedIn = true;
-          if(this.movie?.ownerId === user?._id){
+          if (this.movie?.ownerId === user?._id) {
             this.isOwner = true;
           }
         }
-      })
-    })
-
+      });
+    });
   }
 
-  getNewComment(comments: any){
-      this.comments = comments;
+  getNewComment(comments: any) {
+    this.comments = comments;
   }
 
   deleteHandler(): void {
     this.modalService.open();
   }
 
-  editHandler():void {
-    this.router.navigate([`/movie-edit/${this.movieId}`], { state: { isOwner: this.isOwner } });
+  editHandler(): void {
+    this.router.navigate([`/movie-edit/${this.movieId}`], {
+      state: { isOwner: this.isOwner },
+    });
+  }
+
+  deleteComment(commentId: string): void {
+
+  
+    this.movieService.deleteComment(commentId).subscribe((data) => {
+      console.log("Comment deleted");
+      console.log(data);
+    });
   }
 }
