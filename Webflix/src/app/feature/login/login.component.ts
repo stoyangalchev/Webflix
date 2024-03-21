ca413e408a879c753d60ae6aac07804f0a585b3b
stoyangalchev <galchev98@gmail.com>
2024-03-21 10:13:39 +0200

    Update login.component.ts

--- Webflix/src/app/feature/login/login.component.ts
+++ Webflix/src/app/feature/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { UserService } from '../services/user.service';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UserService } from "../services/user.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
   constructor(private userService: UserService, private router: Router) {}
@@ -17,11 +17,10 @@ export class LoginComponent {
     }
 
     const { username, password } = form.value;
-    
 
     this.userService.login(username, password).subscribe(() => {
-      this.router.navigate(['/home']);
+   
+      this.router.navigate(["/home"]);
     });
   }
-
-} 
+}
