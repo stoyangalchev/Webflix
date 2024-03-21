d6060b3a7ddfe7e7b7198fa573e56c10cb7a45f6
stoyangalchev <galchev98@gmail.com>
2024-03-21 10:13:51 +0200

    Update app.component.ts

--- Webflix/src/app/app.component.ts
+++ Webflix/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../app/feature/services/user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'Webflix';
+export class AppComponent implements OnInit {
+  title = "Webflix";
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    const user = JSON.parse(sessionStorage.getItem("user") ?? "{}");
+    if (user) {
+      this.userService.user$$.next(user);
+    }
+
+   
+  }
 }
