bee5b0bfb1ac6f2297e12e04f26ccd5477cb8d6b
stoyangalchev <galchev98@gmail.com>
2024-03-24 10:33:27 +0200

    Update feature.module.ts

--- Webflix/src/app/feature/feature.module.ts
+++ Webflix/src/app/feature/feature.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import MovieDetailsComponent from './movie-details/movie-details.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { MyProfileComponent } from './my-profile/my-profile.component';
-import { MovieCreateComponent } from './movie-create/movie-create.component';
-import { MovieEditComponent } from './movie-edit/movie-edit.component';
-import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommentComponent } from './comment/comment.component';
-import { ModalComponent } from './modal/modal.component';
-import { AboutComponent } from './about/about.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home/home.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import MovieDetailsComponent from "./movie-details/movie-details.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { MyProfileComponent } from "./my-profile/my-profile.component";
+import { MovieCreateComponent } from "./movie-create/movie-create.component";
+import { MovieEditComponent } from "./movie-edit/movie-edit.component";
+import { SharedModule } from "../shared/shared.module";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { CommentComponent } from "./comment/comment.component";
+import { ModalComponent } from "./modal/modal.component";
+import { AboutComponent } from "./about/about.component";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
   declarations: [
@@ -30,6 +29,7 @@ import { AboutComponent } from './about/about.component';
     CommentComponent,
     ModalComponent,
     AboutComponent,
+    SearchComponent,
   ],
   imports: [CommonModule, SharedModule, RouterModule, FormsModule],
   exports: [
@@ -42,6 +42,8 @@ import { AboutComponent } from './about/about.component';
     MovieDetailsComponent,
     MovieEditComponent,
     CommentComponent,
+    AboutComponent,
+    SearchComponent,
     ModalComponent,
   ],
 })
