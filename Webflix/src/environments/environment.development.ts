0db4d36dd53ae46d94a499ff617941008ab4177a
stoyangalchev <galchev98@gmail.com>
2024-08-23 18:56:16 +0300

    Update package

--- Webflix/src/environments/environment.development.ts
+++ Webflix/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: "http://localhost:3000/api",
+  apiUrl: "https://webflix-jri1.onrender.com",
+  // apiUrl: "http://localhost:3000/api",
 };
--- Webflix/src/environments/environment.ts
+++ Webflix/src/environments/environment.ts
@@ -1,5 +1,1 @@
-export const environment = {
-  
-  apiUrl:
-    "mongodb+srv://stoyangalchev98:stoyan@cluster1.ji8yn.mongodb.net/webflix?retryWrites=true&w=majority&appName=Cluster1",
-};
+export const environment = {};
