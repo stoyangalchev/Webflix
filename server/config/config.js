e3fccbdee051e5f7683a8e091626c930d7e0b69e
stoyangalchev <galchev98@gmail.com>
2024-08-23 19:58:30 +0300

    s

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 const config = {
   // dbURL: "mongodb://localhost:27017/webflix",
   dbURL: process.env.DB_URL_CREDENTIALS,
 
-  origin: ["http://localhost:3001", "http://localhost:4200"],
+  origin: ["*"],
 };
 
 module.exports = config;
