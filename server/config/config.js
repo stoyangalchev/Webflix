0c9c8437aa781c898d1c633e1688e7627ca1a60d
stoyangalchev <galchev98@gmail.com>
2024-08-24 09:32:30 +0300

    Update server

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,7 @@ const config = {
   // dbURL: "mongodb://localhost:27017/webflix",
   dbURL: process.env.DB_URL_CREDENTIALS,
 
-  credentials: true,
   origin: ["https://webflix-chi.vercel.app"],
-  allowedHeaders: ["Content-Type", "X-Authorization"],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
 
 module.exports = config;
